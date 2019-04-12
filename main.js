4b224c54e988a19fe7368e73f708e25b043736ad
Riho-Kitagawa <45659575+Riho-Kitagawa@users.noreply.github.com>
2019-04-12 23:51:47 +0900

    Second commit

---
+++ main.js
@@ -0,0 +1,73 @@
+// Responsive Nav
+
+$(function() {
+  menu = $("nav ul");
+
+  $("#openup").on("click", function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+
+  $(window).resize(function() {
+    var w = $(this).width();
+    if (w > 480 && menu.is(":hidden")) {
+      menu.removeAttr("style");
+    }
+  });
+
+  $("nav li").on("click", function(e) {
+    var w = $(window).width();
+    if (w < 480) {
+      menu.slideToggle();
+    }
+  });
+  $(".open-menu").height($(window).height());
+});
+
+//smooth scrolling
+$(".cf a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+$(".totop").on("click", function(event) {
+  if (this.hash !== "") {
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+$(document).ready(function() {
+  var pagetop = $(".pagetop");
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function() {
+    $("body, html").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+});
